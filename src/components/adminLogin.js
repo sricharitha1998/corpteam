1d323978da3e36519b82275dbba8863e360055df
sricharitha1998 <91675361+sricharitha1998@users.noreply.github.com>
2024-08-06 16:29:59 +0530

    Update adminLogin.js

--- src/components/adminLogin.js
+++ src/components/adminLogin.js
@@ -80,7 +80,7 @@ const Login = async (event) => {
                         </div>
                         
                         <div className="text-center">
-                          <button type="submit" onClick={Login} className="btn btn-primary btn-block">Sign In</button>
+                          <button type="submit" onClick={Login} className="btn btnColor text-white btn-block">Sign In</button>
                         </div>
                       </form>
                     </div>

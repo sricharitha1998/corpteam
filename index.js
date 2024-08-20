c2bf3ec5189f8b7fe20f7d96e2426008a066239f
root <root@ubuntu-cloudpanel.localhost>
2024-08-20 06:18:18 +0000

    changes in vps

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
+const httpApp = express();
 const app = express();
 app.use(express.json());
 app.use(cors())
+const https = require('https');
+const fs = require('fs');
 app.use(express.urlencoded({extended: true}));
 require('./DB/conn');
 const userModel = require('./models/userModel');
@@ -13,16 +16,30 @@ const path = require('path');
 app.use(bodyParser.urlencoded({ limit: '5000mb', extended: true }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-};
-app.use(cors(corsOptions));
+//app.use(cors());
+
+app.use(cors({
+    origin: '*'
+}));
 
 const PORT = 4000;
+const HOST = '0.0.0.0';
 
 const users = require('./routes/userRoute');
 app.use('/users/', users);
+
+
+// Load the private key and certificate
+const sslOptions = {
+  key: fs.readFileSync('./ssl/361366def475b285.key'),
+  cert: fs.readFileSync('./ssl/361366def475b285.crt'),
+  ca: fs.readFileSync('./ssl/gd_bundle-g2-g1.crt')
+};
+
+//const sslOptions = {
+  // key: fs.readFileSync(path.join(__dirname, 'ssl-certificates/privatekey.pem')),
+    //cert: fs.readFileSync(path.join(__dirname, 'ssl-certificates/certificate.pem'))
+//};
 
 const workClosure = require('./routes/workClosureRoute');
 app.use('/workClosure/', workClosure);
@@ -60,8 +77,16 @@ app.get('/public/invoice/:filename', (req, res) => {
     });
 });
 
-app.listen(PORT, async function (res) { 
-    console.log(`server started at ${PORT}`);
+app.get('/welcome', (req, res) => {
+    res.status(200).send('Welcome to the API!');
+});
+
+// Create an HTTPS server
+//https.createServer(sslOptions, app).listen(PORT, '0.0.0.0', async () => {
+app.listen(PORT, HOST,  async function (res) { 
+    console.log(`HTTPS Server running on port ${PORT}`);
+
+console.log(`server started at ${PORT}`);
     const adminDetails = {
         email: 'admin@gmail.com',
         role: "admin",
@@ -79,5 +104,5 @@ app.listen(PORT, async function (res) {
     } else {
         console.log("Admin already exists");
     }
-})
- 
+
+});

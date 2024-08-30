05a1442771a3676fae0b798e9372acf0def120ab
sricharitha1998 <sricharitha1998@gmail.com>
2024-08-30 15:51:33 +0530

    updates

--- src/components/purchaseOrder.js
+++ src/components/purchaseOrder.js
@@ -354,7 +354,7 @@ function PurchaseOrder() {
 
                                     <div className="row">
                                         <div className="col-sm-6">
-                                            <a href="javascript:void(0);" className="btn btn-success text-white mb-2" onClick={handleAddService}>
+                                            <a href="javascript:void(0);" className="btn btnColor text-white mb-2" onClick={handleAddService}>
                                                 Add Services<svg className="ms-4 scale3" width="16" height="16" viewbox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.9638 11.5104L16.9721 14.9391L3.78954 1.7565C3.22815 1.19511 2.31799 1.19511 1.75661 1.7565C1.19522 2.31789 1.19522 3.22805 1.75661 3.78943L14.9392 16.972L11.5105 16.9637L11.5105 16.9637C10.7166 16.9619 10.0715 17.6039 10.0696 18.3978C10.0677 19.1919 10.7099 19.8369 11.5036 19.8388L11.5049 19.3388L11.5036 19.8388L18.3976 19.8554L18.4146 19.8555L18.4159 19.8555C18.418 19.8555 18.42 19.8555 18.422 19.8555C19.2131 19.8533 19.8528 19.2114 19.8555 18.4231C19.8556 18.4196 19.8556 18.4158 19.8556 18.4117L19.8389 11.5035L19.8389 11.5035C19.8369 10.7097 19.1919 10.0676 18.3979 10.0695C17.604 10.0713 16.9619 10.7164 16.9638 11.5103L16.9638 11.5104Z" fill="white" stroke="white"></path>
                                                 </svg>
                                             </a>
@@ -418,7 +418,7 @@ function PurchaseOrder() {
                                 </div>
                                 <div className="row">
                                     <div className="col-sm-6">
-                                        <a href="javascript:void(0);" className="btn btn-success text-white mb-2" onClick={handleAddSupply}>
+                                        <a href="javascript:void(0);" className="btn btnColor text-white mb-2" onClick={handleAddSupply}>
                                             Add Supply<svg className="ms-4 scale3" width="16" height="16" viewbox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.9638 11.5104L16.9721 14.9391L3.78954 1.7565C3.22815 1.19511 2.31799 1.19511 1.75661 1.7565C1.19522 2.31789 1.19522 3.22805 1.75661 3.78943L14.9392 16.972L11.5105 16.9637L11.5105 16.9637C10.7166 16.9619 10.0715 17.6039 10.0696 18.3978C10.0677 19.1919 10.7099 19.8369 11.5036 19.8388L11.5049 19.3388L11.5036 19.8388L18.3976 19.8554L18.4146 19.8555L18.4159 19.8555C18.418 19.8555 18.42 19.8555 18.422 19.8555C19.2131 19.8533 19.8528 19.2114 19.8555 18.4231C19.8556 18.4196 19.8556 18.4158 19.8556 18.4117L19.8389 11.5035L19.8389 11.5035C19.8369 10.7097 19.1919 10.0676 18.3979 10.0695C17.604 10.0713 16.9619 10.7164 16.9638 11.5103L16.9638 11.5104Z" fill="white" stroke="white"></path>
                                             </svg>
                                         </a>
